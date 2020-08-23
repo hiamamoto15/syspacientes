bbd2a83f7b69df87fb74329de948c3ee7980aaca
Your Name <you@example.com>
2020-08-23 19:58:23 -0300

    validação

--- redirect.php
+++ redirect.php
@@ -2,6 +2,13 @@
     $email = $_POST["inputEmail"];
     $senha = $_POST["inputPassword"];
 
+    if(strlen($senha) < 6 ){
+        echo "Senha menor que 6 caracteres";
+    }
+    else if(strlen($senha) > 7) {
+        echo "Senha maior que 6 caracteres";
+    }
+    echo "<hr>";
     echo "Email: " . $email. "<br>";
     echo "Senha: " . $senha;
 
