7f96cf5a4067ccb5296b7274eb27b83c0eb50443
Your Name <you@example.com>
2020-08-23 20:03:25 -0300

    validação senha e retorno

--- redirect.php
+++ redirect.php
@@ -4,12 +4,18 @@
 
     if(strlen($senha) < 6 ){
         echo "Senha menor que 6 caracteres";
+        $validou = false;
     }
-    else if(strlen($senha) > 7) {
+    else if(strlen($senha) > 6) {
         echo "Senha maior que 6 caracteres";
+        $validou = false;
     }
+    if($validou) {
     echo "<hr>";
     echo "Email: " . $email. "<br>";
     echo "Senha: " . $senha;
+    }else {
+        header("location:index.php");
+    }
 
 ?>
