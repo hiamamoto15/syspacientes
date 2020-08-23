d7e6a6bab66f1ce9ce2c549efb0df22519da5119
Your Name <you@example.com>
2020-08-23 20:56:13 -0300

    conexao bd

--- redirect.php
+++ redirect.php
@@ -8,6 +8,17 @@
     $senha = $_POST["inputPassword"];
     $validou = true;
     $erro = " ";
+
+    //validar login
+
+    $sql = "SELECT * FROM usuarios WHERE email = '$email'";
+    $resp =mysqli_query($conexao_bd, $sql);
+
+    if($rows=mysqli_fatch_row($resp)){
+        echo $rows[0] . " | " . $rows[1] . " | " . $rows[2];
+    }
+    mysqli_close($conexao_bd);
+
     if(strlen($senha) < 6 ){
         $erro = "Senha menor que 6 caracteres";
         $validou = false;
