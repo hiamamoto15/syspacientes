e6304600aa816f9ce115aafa41a4425f1ec6c703
Your Name <you@example.com>
2020-08-24 19:46:52 -0300

    edit geral

--- admin.php
+++ admin.php
@@ -5,7 +5,7 @@
     $id_usuario = $GET_["id_usuario"];
     $nome_usuario = "";
 
-    $sql = "SELECT nome * FROM usuarios WHERE id = " . $id_usuario;
+    $sql = "SELECT nome FROM usuarios WHERE id = " . $id_usuario;
     $resp = mysqli_query($conexao_bd, $sql);
     if($rows=mysqli_fetch_row($resp)) {
         $nome_usuario = $rows[0];
@@ -22,6 +22,7 @@
     <title>SysPacientes - ADM </title>
     <link rel='icon' href="img/favicon/favicon2.ico">
     <link href="css/bootstrap.min.css" rel="stylesheet">
+    
     
         
 </head>
@@ -45,6 +46,21 @@
         <li class="active"><a href="#">Home</a></li>
         <li><a href="#">Contato</a></li>
         <li><a href="#">Sobre</a></li>
+        <li class="dropdown">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Outros <span class="caret"></span></a>
+          <ul class="dropdown-menu">
+            <li><a href="#">Ações</a></li>
+            <li><a href="#">Serviços</a></li>
+            <li><a href="#">Valores</a></li>
+            <li role="separator" class="divider"></li>
+
+          </ul>
+        </li>
+      </ul>
+      <ul class="nav navbar-nav navbar-right">
+        <li class="active"><a href="./">Receitas <span class="sr-only">(current)</span></a></li>
+        <li><a href="../navbar-static-top/">Comprar </a></li>
+        <li><a href="../navbar-fixed-top/">Encerrar sessão</a></li>
       </ul>
     </div><!--/.nav-collapse -->
   </div><!--/.container-fluid -->
@@ -52,18 +68,26 @@
 
 <!-- Main component for a primary marketing message or call to action -->
 <div class="jumbotron">
-  <h1>
-  <?php 
-    echo "<h3>Bem vindo: ". $nome_usuario . "</h3> ";
-    ?>
-  </h1>
-  <p>Esta é a sua pagina principal. Aqui voce pode.......</p>
+  <h1><?php echo "<h3>Bem vindo: ". $nome_usuario . "</h3> "; ?></h1>
+  <p>Esta é sua pagina principal, nela você pode...</p>
   <p>
-    <a class="btn btn-lg btn-primary" href="../../components/#navbar" role="button"> Clique para saber mais &raquo;</a>
+    <a class="btn btn-lg btn-primary" href="https://www.youtube.com/watch?v=dQw4w9WgXcQ" role="button">Clique para saber mais &raquo;</a>
   </p>
 </div>
 
 </div> <!-- /container -->
-       
+
+
+<!-- Bootstrap core JavaScript
+================================================== -->
+<!-- Placed at the end of the document so the pages load faster -->
+<script src="https://code.jquery.com/jquery-1.12.4.min.js" integrity="sha384-nvAa0+6Qg9clwYCGGPpDQLVpLNn0fRaROjHqs13t4Ggj3Ez50XnGQqc/r8MhnRDZ" crossorigin="anonymous"></script>
+<script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery.min.js"><\/script>')</script>
+<script src="../../dist/js/bootstrap.min.js"></script>
+<!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+<script src="../../assets/js/ie10-viewport-bug-workaround.js"></script>
+    
+
+    
 </body>
 </html>
