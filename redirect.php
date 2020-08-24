409921d74d2cbfaae8f3f98526dc9afe5b9902b5
Your Name <you@example.com>
2020-08-23 21:23:19 -0300

    s

--- redirect.php
+++ redirect.php
@@ -12,7 +12,7 @@
 
     //validar login
 
-    $sql = "SELECT id, email, senha * FROM usuarios WHERE email = '$email'";
+    $sql = "SELECT * FROM usuarios WHERE email = '$email'";
     $resp =mysqli_query($conexao_bd, $sql);
 
     if($rows=mysqli_fetch_row($resp)){
@@ -30,14 +30,7 @@
     }
     mysqli_close($conexao_bd);
 
-    if(strlen($senha) < 6 ){
-        $erro = "Senha menor que 6 caracteres";
-        $validou = false;
-    }
-    else if(strlen($senha) > 6) {
-        $erro = "Senha maior que 6 caracteres";
-        $validou = false;
-    }
+
     if($validou) {
     echo "<hr>";
     echo "Email: " . $email. "<br>";
