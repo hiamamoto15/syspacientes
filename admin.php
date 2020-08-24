75d4722487c99927a4775c7816c945b54172d0f6
Your Name <you@example.com>
2020-08-24 19:18:54 -0300

    editando navbar

--- admin.php
+++ admin.php
@@ -38,30 +38,18 @@
               <span class="icon-bar"></span>
               <span class="icon-bar"></span>
             </button>
-            <a class="navbar-brand" href="#">Project name</a>
+            <a class="navbar-brand" href="#">SysPacientes</a>
           </div>
           <div id="navbar" class="navbar-collapse collapse">
             <ul class="nav navbar-nav">
               <li class="active"><a href="#">Home</a></li>
-              <li><a href="#">About</a></li>
-              <li><a href="#">Contact</a></li>
-              <li class="dropdown">
-                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
-                <ul class="dropdown-menu">
-                  <li><a href="#">Action</a></li>
-                  <li><a href="#">Another action</a></li>
-                  <li><a href="#">Something else here</a></li>
-                  <li role="separator" class="divider"></li>
-                  <li class="dropdown-header">Nav header</li>
-                  <li><a href="#">Separated link</a></li>
-                  <li><a href="#">One more separated link</a></li>
-                </ul>
-              </li>
+              <li><a href="#">Contato</a></li>
+              <li><a href="#">Sobre</a></li>
             </ul>
             <ul class="nav navbar-nav navbar-right">
-              <li class="active"><a href="./">Default <span class="sr-only">(current)</span></a></li>
-              <li><a href="../navbar-static-top/">Static top</a></li>
-              <li><a href="../navbar-fixed-top/">Fixed top</a></li>
+              <li class="active"><a href="./">Status <span class="sr-only">(current)</span></a></li>
+              <li><a href="../navbar-static-top/">Receitas</a></li>
+              <li><a href="../navbar-fixed-top/">Situação</a></li>
             </ul>
           </div><!--/.nav-collapse -->
         </div><!--/.container-fluid -->
@@ -69,19 +57,19 @@
 
       <!-- Main component for a primary marketing message or call to action -->
       <div class="jumbotron">
-        <h1>Navbar example</h1>
-        <p>This example is a quick exercise to illustrate how the default, static navbar and fixed to top navbar work. It includes the responsive CSS and HTML, so it also adapts to your viewport and device.</p>
+        <h1>
+        <?php
+          echo "<h3>Bem vindo: ". $nome_usuario . "</h3> ";
+        ?>
+        </h1>
+        <p>Esta plaforma está em manutenção! Aguarde por novos procedimentos!!.</p>
         <p>
-          <a class="btn btn-lg btn-primary" href="../../components/#navbar" role="button">View navbar docs &raquo;</a>
+          <a class="btn btn-lg btn-primary" href="../../components/#navbar" role="button">Acessar &raquo;</a>
         </p>
       </div>
 
     </div>
-    <?php
-    
-    echo "<h3>Bem vindo: ". $nome_usuario . "</h3> ";
-    
-    ?>
+
     
 </body>
 </html>
