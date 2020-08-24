446e1b345504f2dc46fe767e79443a88817e90af
Your Name <you@example.com>
2020-08-24 20:10:04 -0300

    .

--- admin.php
+++ admin.php
@@ -35,7 +35,33 @@
     <div class="navbar-header">
       <a class="navbar-brand" href="#">SysPacientes</a>
     </div>
-   
+    <div id="navbar" class="navbar-collapse collapse">
+      <ul class="nav navbar-nav">
+        <li class="active"><a href="#">Home</a></li>
+        <li><a href="#">Contato</a></li>
+        <li><a href="#">Sobre</a></li>
+        <li class="dropdown">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Outros <span class="caret"></span></a>
+          <ul class="dropdown-menu">
+            <li><a href="#">Ações</a></li>
+            <li><a href="#">Serviços</a></li>
+            <li><a href="#">Valores</a></li>
+            <li role="separator" class="divider"></li>
+            <li class="dropdown-header">Nav header</li>
+                  <li><a href="#">Separated link</a></li>
+                  <li><a href="#">One more separated link</a></li>
+
+          </ul>
+        </li>
+      </ul>
+      <ul class="nav navbar-nav navbar-right">
+        <li class="active"><a href="./">Receitas <span class="sr-only">(current)</span></a></li>
+        <li><a href="../navbar-static-top/">Comprar </a></li>
+        <li><a href="../navbar-fixed-top/">Encerrar sessão</a></li>
+      </ul>
+    </div><!--/.nav-collapse -->
+  </div><!--/.container-fluid -->
+</nav>
 
 <!-- Main component for a primary marketing message or call to action -->
 <div class="jumbotron">
