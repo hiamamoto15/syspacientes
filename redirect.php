373d4a9cea9252ebf46a5fb55d04e306745c0c1e
Your Name <you@example.com>
2020-09-26 14:32:09 -0300

    novos arquivos

--- redirect.php
+++ redirect.php
@@ -5,26 +5,38 @@
     $email = $_POST["inputEmail"];
     $senha = $_POST["inputPassword"];
 
+    //validar variavel
+    if(strlen(trim($email)) == 0 &&
+      strlen(trim($senha)) == 0){
+      header("location: index.php");
+   }
     //iniciando sessao
     session_start();
     $_SESSION['$id_usuario'] = 0;
+    $_SESSION["tipo_acesso"] = 2;
 
 
     $validou = false;
     $erro = "Nenhuma credencial encontrada";
     $id_usuario = 0;
+    $tipo_acesso = 2;
 
     //validar login
 
-    $sql = "SELECT id, email, senha FROM usuarios WHERE email = '$email'";
-    $resp =mysqli_query($conexao_bd, $sql);
-
-    if($rows=mysqli_fetch_row($resp)){
-        if($senha == $rows[2]){
-            $erro = "";
-            $validou = true;
-            $id_usuario = $rows[0];
-        }
+    $sql = "SELECT id, email, senha, tipo_acesso FROM usuarios WHERE email = '$email'";
+    $resp = mysqli_query($conexao_bd, $sql);
+    if($rows=mysqli_fetch_row($resp)){      
+       if($senha == $rows[2]){
+          $erro        = "";
+          $validou     = true;
+          $id_usuario  = $rows[0];
+          $tipo_acesso = $rows[3];
+       }else{
+          $erro    = "Credenciais inválidas!";
+          $validou = false;
+       }
+    }
+    mysqli_close($conexao_bd);
         else {
             $erro = " Credenciais inválidas!";
             $validou = false;
@@ -35,16 +47,13 @@
     mysqli_close($conexao_bd);
 
 
-    if($validou) {
-    /* echo "<hr>";
-    echo "Email: " . $email. "<br>";
-    echo "Senha: " . $senha;
-    */
-    $_SESSION["id+usuario"] = $id_usuario;
-    header("location:admin.php?id_usuario=$id_usuario");
+    if($validou){
 
-    }else {
+        $_SESSION["id_usuario"]  = $id_usuario;
+        $_SESSION["tipo_acesso"] = $tipo_acesso;
+        header("location:admin.php");
+     }else{
         header("location:index.php?erro=$erro");
-    }
+     }
 
 ?>
