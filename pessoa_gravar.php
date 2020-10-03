0dd2c553422c184f348cdfae98cf0e40efd87bf4
Your Name <you@example.com>
2020-10-03 17:03:41 -0300

    .

--- pessoa_gravar.php
+++ pessoa_gravar.php
@@ -16,31 +16,15 @@
    $celularPessoa       = $_POST["inputCell"];
    $emailPessoa         = $_POST["inputEmail"];
    
-   if(strlen($id_pessoa) > 0){
-         if($id_pessoa){
-            $sql = "UPDATE pessoas SET 
-            nome             ='$nomePessoa', 
-            endereco         ='$endPessoa',
-            numero           ='$numPessoa',
-            complemento      ='$complePessoa',
-            cidade           ='$cidadePessoa',
-            estado           ='$estadoPessoa',
-            cep              ='$cepPessoa',
-            datanascimento   ='$dtnascimentoPessoa',
-            telefone         ='$telefonePessoa',
-            celular          ='$celularPessoa',
-            email            ='$emailPessoa'
-           WHERE idPessoa = $id_pessoa";
+
+
       
-   }else
+ 
          $sql = "INSERT INTO pessoas (nome,endereco,numero,complemento,cidade,estado,cep,datanascimento,telefone,celular,email)
                                VALUES('$nomePessoa','$endPessoa','$numPessoa','$complePessoa','$cidadePessoa','$estadoPessoa',
                                '$cepPessoa','$dtnascimentoPessoa','$telefonePessoa','$celularPessoa','$emailPessoa')";
-      }
-      mysqli_query($conexao_bd, $sql);
-   }else{
-      //erro!
-   }
+
+   mysqli_query($conexao_bd, $sql);
    mysqli_close($conexao_bd);
    header("location:pessoa_list2.php");
    
