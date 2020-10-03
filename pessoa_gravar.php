b7c4d9b57d94f0c32cc890ebeca5f372481c56fc
Your Name <you@example.com>
2020-10-03 15:13:17 -0300

    .

--- pessoa_gravar.php
+++ pessoa_gravar.php
@@ -18,7 +18,7 @@
    $celularPessoa       = $_POST["inputCell"];
    $emailPessoa         = $_POST["inputEmail"];
    
-   if(strlen($id_usuario) > 0){
+   if(strlen($id_pessoa) > 0){
          if($id_pessoa){
             $sql = "UPDATE pessoas SET 
             nome             ='$nomePessoa', 
